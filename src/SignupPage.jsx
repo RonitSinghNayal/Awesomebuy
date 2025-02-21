a058938fbdb44bfb1e747be14acf67204442083d
Ronit Singh Nayal <ronitsinghnayal@gmail.com>
2025-02-21 21:33:58 +0530

    handlechangecount error

--- src/SignupPage.jsx
+++ src/SignupPage.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 function SignupPage() {
   return (
-    <>
-     This is Signup SignupPage 
-    </>
+    <h1 className='text-3xl'>
+     This is SignupPage
+    </h1>
   )
 }
 
 export default SignupPage
+    
